e2dd6254b178dbcb6a6e98480f8d01524f7a4d2c
Jitto Joseph <78639807+JittoJoseph@users.noreply.github.com>
2024-10-26 13:38:30 +0530

    added custom app icon and changed name

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import 'package:stakingnotify/main.dart';
+import 'package:prestake/main.dart';
 import 'account.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
